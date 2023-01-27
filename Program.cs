5165014467453869f5861aa45827c0e28ad801e5
Dennis Kjellin <denniskjellin@hotmail.com>
2023-01-27 15:34:18 +0100

    Activating MVC, setting up usage of static files and use of routing, then setting up own routing with a set name and pattern.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews(); // Activate MVC
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseStaticFiles(); // use of static files
+app.UseRouting(); // use routing 
+
+// Create routing, set name & pattern
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}"
+);
 
 app.Run();
