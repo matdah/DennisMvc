98ae2cee36f93ef28f8f2a27def1f5a28a20f630
Dennis Kjellin <denniskjellin@hotmail.com>
2023-01-31 21:51:48 +0100

    Cant read JSON file :(

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
 using DennisMvc.Models;
+using Newtonsoft.Json;
+
+
+
 
 
 namespace DennisMvc.Controllers
@@ -14,9 +17,7 @@ namespace DennisMvc.Controllers
     
         public IActionResult Index()
         {
-            // read JSON
             var jsonStr = System.IO.File.ReadAllText("destinations.json");
-            // convert json
             var JsonObj = JsonConvert.DeserializeObject<IEnumerable<Destinations>>(jsonStr);
             return View(JsonObj);
         }
