a7a8c71edc90c6e3a74aee12abd79e5eb6a914ff
Dennis Kjellin <denniskjellin@hotmail.com>
2023-01-31 16:25:31 +0100

    created new page and changed the routing of index and about page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,7 +8,14 @@ namespace DennisMvc.Controllers
 {
     public class HomeController : Controller
     {
+        [Route("/home")]
         public IActionResult Index()
+        {
+            return View();
+        }
+
+       [Route("/about")]
+        public IActionResult About()
         {
             return View();
         }
