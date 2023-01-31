a25ac17590044a62e3c3de353c529635d28134c5
Dennis Kjellin <denniskjellin@hotmail.com>
2023-01-31 17:28:28 +0100

    Creating a model for Destinations

---
+++ Models/Destinations.cs
@@ -0,0 +1,11 @@
+using System;
+namespace DennisMvc.Models
+{
+    public class Destinations {
+        // Properties
+        public int Id { get; set; }
+
+        public String? DestinationName { get; set; }
+        public String? DestinationUrl { get; set; }
+    }
+}
