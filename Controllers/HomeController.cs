d3d86bca1952c011e8ecf5c52eede974f1e1a7a8
Dennis Kjellin <denniskjellin@hotmail.com>
2023-02-02 16:46:55 +0100

    Writing out my list with ViewBag

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -14,27 +14,28 @@ namespace DennisMvc.Controllers
 {
     public class HomeController : Controller
     {
-    
+
         public IActionResult Index()
         {
-           ViewBag.Heading = "ViewBag & ViewData";
-           ViewBag.Text = "This heading and text is brought to you by the ViewBag property."; 
-           ViewData["ErrorMessage"] = "This example of a 'Error message' is brought to you from the ViewData!";
-           return View();
+            ViewBag.Heading = "ViewBag & ViewData";
+            ViewBag.Text = "This heading and text is brought to you by the ViewBag property.";
+            ViewData["ErrorMessage"] = "This example of a 'Error message' is brought to you from the ViewData!";
+            return View();
         }
 
-       [Route("/about")]
+        [Route("/about")]
         public IActionResult About()
         {
             return View();
         }
 
-         [Route("/destinations")]
+        [Route("/destinations")]
         public IActionResult Destinations()
         {
             ViewBag.Destinations = "Whenever I have ben to one of the great Alp destinations I can add a destination to my list throug the form below! (This description is presented with 'ViewBag').";
             var jsonStr = System.IO.File.ReadAllText("destinations.json");
-            var JsonObj = JsonConvert.DeserializeObject<IEnumerable<Destinations>>(jsonStr);
+            var JsonObj = JsonConvert.DeserializeObject<List<Destinations>>(jsonStr);
+            ViewBag.JsonObj = JsonObj;
             return View(JsonObj);
         }
     }
